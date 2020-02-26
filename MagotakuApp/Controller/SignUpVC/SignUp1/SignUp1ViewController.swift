1991765d7419f1e7fdb7c3143598f97c47ba4c5e
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-02-26 15:03:28 +0900

    signUpのUIを整えつつある

--- MagotakuApp/Controller/SignUpVC/SignUp1/SignUp1ViewController.swift
+++ MagotakuApp/Controller/SignUpVC/SignUp1/SignUp1ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class SignUp1ViewController: UIViewController {
-
+class SignUp1ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    //サービス利用者
+    let serviceLastName: CustomTextField! = CustomTextField()
+    let serviceFirstName: CustomTextField! = CustomTextField()
+    //アプリ利用者
+    let appLastName: CustomTextField! = CustomTextField()
+    let appFirstName: CustomTextField! = CustomTextField()
+    
+    
+    
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "利用者情報"
+        
+        self.serviceLastName.delegate = self
+        self.serviceLastName.delegate = self
         
         //　ナビゲーションバーの背景色
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 23/255, green: 35/255, blue: 57/255, alpha: 1)
@@ -25,21 +42,44 @@ class SignUp1ViewController: UIViewController {
             .foregroundColor: UIColor.white
         ]
         
+        
+        //TFの配置
+//        self.serviceLastName.frame = CGRect(x: 32, y: 192 , width: (UIScreen.main.bounds.size.width - 80) / 2 , height: 48)
+//        self.serviceFirstName.frame = CGRect(x: 48 + (UIScreen.main.bounds.size.width - 80) / 2 , y: 192 , width: (UIScreen.main.bounds.size.width - 80) / 2, height: 48)
+        self.serviceLastName.placeholder = "姓"
+        self.serviceFirstName.placeholder = "名前"
+        self.serviceLastName.backgroundColor = UIColor(red: 232/255, green: 234/255, blue: 240/255, alpha: 1)
+        self.serviceFirstName.backgroundColor = UIColor(red: 232/255, green: 234/255, blue: 240/255, alpha: 1)
+        self.serviceLastName.layer.cornerRadius = 6
+        self.serviceFirstName.layer.cornerRadius = 6
+        self.view.addSubview(self.serviceFirstName)
+        self.view.addSubview(self.serviceLastName)
+        
+        nextBtn.layer.cornerRadius = 24.0
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let NavH:CGFloat? = navigationController?.navigationBar.frame.size.height
+        let topM:CGFloat? = self.view.safeAreaInsets.top
+        print(NavH!)
+        print(topM!)
+        
+        self.serviceLastName.frame = CGRect(x: 32, y: 72 + topM! + NavH! , width: (UIScreen.main.bounds.size.width - 80) / 2 , height: 48)
+        self.serviceFirstName.frame = CGRect(x: 48 + (UIScreen.main.bounds.size.width - 80) / 2 , y: 72 + topM! + NavH! , width: (UIScreen.main.bounds.size.width - 80) / 2, height: 48)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = false
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
