97005b1225cd4a37a02cbb390deb4416ae509f9d
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-03-06 21:01:11 +0900

    カレンダーの設定

--- MagotakuApp/Controller/Home/Reservation01/Reservation01ViewController.swift
+++ MagotakuApp/Controller/Home/Reservation01/Reservation01ViewController.swift
@@ -7,26 +7,108 @@
 //
 
 import UIKit
+import Firebase
+import FSCalendar
 
-class Reservation01ViewController: UIViewController {
-
+class Reservation01ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
+    
+    
+    //カレンダー定義
+    fileprivate weak var calendar: FSCalendar!
+    var titleLabel = UILabel()
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    var selectedDate: [Int]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //NavigationBarのタイトル設定
-        self.title = "新規予約 1"
+        self.title = "訪問希望日を選ぶ"
+        
+        //titleLabelの設定
+//        titleLabel.text = ""
+//        titleLabel.textAlignment = .left
+//        titleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+//        view.addSubview(titleLabel)
+        
+        
+        //カレンダーの設定について
+        let calendar = FSCalendar()
+        calendar.dataSource = self
+        calendar.delegate = self
+        view.addSubview(calendar)
+        self.calendar = calendar
+        //カレンダーの表記を日本語化
+        calendar.appearance.headerDateFormat = "YYYY年MM月"
+        self.calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
+        self.calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
+        self.calendar.calendarWeekdayView.weekdayLabels[2].text = "火"
+        self.calendar.calendarWeekdayView.weekdayLabels[3].text = "水"
+        self.calendar.calendarWeekdayView.weekdayLabels[4].text = "木"
+        self.calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
+        self.calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
+        //曜日毎にテキストカラーを変更
+        self.calendar.calendarWeekdayView.weekdayLabels[0].textColor = UIColor.red
+        self.calendar.calendarWeekdayView.weekdayLabels[1].textColor = UIColor.black
+        self.calendar.calendarWeekdayView.weekdayLabels[2].textColor = UIColor.black
+        self.calendar.calendarWeekdayView.weekdayLabels[3].textColor = UIColor.black
+        self.calendar.calendarWeekdayView.weekdayLabels[4].textColor = UIColor.black
+        self.calendar.calendarWeekdayView.weekdayLabels[5].textColor = UIColor.black
+        self.calendar.calendarWeekdayView.weekdayLabels[6].textColor = UIColor.blue
+        
+        //次へボタンの設定
+        nextBtn.layer.cornerRadius = 24
+        nextBtn.backgroundColor = UIColor.gray
+        nextBtn.isEnabled = false
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        let safeAreaHeight = self.view.safeAreaInsets.top
+        let tabbarHeight = self.tabBarController?.tabBar.frame.height
+        print(safeAreaHeight)
+        
+//        titleLabel.frame = CGRect(x: 8, y: safeAreaHeight + 16, width: UIScreen.main.bounds.width - 64, height: 22)
+        calendar.frame = CGRect(x: 0, y: safeAreaHeight + 16 , width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - safeAreaHeight - tabbarHeight! - 60)
+        nextBtn.frame = CGRect(x: 32, y: UIScreen.main.bounds.height - tabbarHeight! - 60, width: UIScreen.main.bounds.width - 64, height: 48)
+        
         
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        //ボタンを使用可能にして、色を変える
+        nextBtn.isEnabled = true
+        nextBtn.backgroundColor = UIColor(red: 124/255, green: 143/255, blue: 230/255, alpha: 1)
+        //選択した日付を変数に代入する
+        selectedDate = getDay(date)
+        if selectedDate != nil{
+            print(selectedDate!)
+        }else{
+            showErrorAlert(text: "日付が選択されてないよ！")
+        }
+        
+        
     }
-    */
+    
+    // date型 -> 年月日をIntで取得
+    func getDay(_ date:Date) -> [Int]{
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        let date: [Int] = [year, month, day]
+        return date
+    }
+
+    @IBAction func tapToNext(_ sender: Any) {
+        let vc = Reservation02ViewController()
+        let backButtonItem = UIBarButtonItem(title: "戻る", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backButtonItem
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+  
 
 }
---
+++ MagotakuApp/Controller/Home/Reservation02/Reservation02ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Reservation02ViewController.swift
+//  MagotakuApp
+//
+//  Created by 宮本一成 on 2020/03/06.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class Reservation02ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
