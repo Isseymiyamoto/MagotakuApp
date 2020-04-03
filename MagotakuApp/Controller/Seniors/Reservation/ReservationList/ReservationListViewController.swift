647a1e8884852bef74508cfa7f4ecf5e44ea55a9
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-04-03 15:48:19 +0900

    予約0件時のview作成

--- MagotakuApp/Controller/Seniors/Reservation/ReservationList/ReservationListViewController.swift
+++ MagotakuApp/Controller/Seniors/Reservation/ReservationList/ReservationListViewController.swift
@@ -36,10 +36,46 @@ class ReservationListViewController: UIViewController, UITableViewDelegate, UITa
         
         tableView.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
         
+        if ReservationCollection.shared.reservationCount() == 0{
+            //予約0件の場合のみ表示する
+            setZeroReservation()
+        }
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         tableView.reloadData()
+    }
+    
+    
+    //予約0件の時のみ表示するメソッド
+    func setZeroReservation(){
+        let view = UIView(frame: self.view.bounds)
+        view.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
+        self.view.addSubview(view)
+        //「確定している予約 0件」表示用のラベル
+        let zeroLable = UILabel()
+        zeroLable.text = "確定している予約 0件"
+        zeroLable.font = UIFont.boldSystemFont(ofSize: 16)
+        zeroLable.frame = CGRect(x: 24, y: 24, width: UIScreen.main.bounds.width - 16, height: 22)
+        view.addSubview(zeroLable)
+        //予約を促す ラベル
+        let stimulatorLabel = UILabel()
+        stimulatorLabel.text = "次の予約を行いましょう"
+        stimulatorLabel.font = UIFont.systemFont(ofSize: 14)
+        stimulatorLabel.frame = CGRect(x: 24, y: 54, width: UIScreen.main.bounds.width - 16, height: 22)
+        view.addSubview(stimulatorLabel)
+        //予約を行うためのボタン
+        let reservationBtn = UIButton(type: .system)
+        reservationBtn.setTitle("新規予約を行う", for: .normal)
+        reservationBtn.setTitleColor(.white, for: .normal)
+        reservationBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        reservationBtn.titleLabel?.textAlignment = .center
+        reservationBtn.backgroundColor = UIColor(red: 94/255, green: 123/255, blue: 211/255, alpha: 1)
+        reservationBtn.frame = CGRect(x: 24, y: 88, width: 240, height: 48)
+        reservationBtn.layer.cornerRadius = 6.0
+        view.addSubview(reservationBtn)
+        
     }
 
 
