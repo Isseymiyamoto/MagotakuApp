624a275b89836500a04866750b2c248443c71314
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-04-03 12:10:00 +0900

    tabbar,tintcolorを変更

--- MagotakuApp/Controller/Seniors/Login/LoginViewController.swift
+++ MagotakuApp/Controller/Seniors/Login/LoginViewController.swift
@@ -52,7 +52,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         // navigationBarのタイトル
         self.title = "ログイン"
         //　ナビゲーションバーの背景色
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 23/255, green: 35/255, blue: 57/255, alpha: 1)
+//        self.navigationController?.navigationBar.barTintColor = UIColor(red: 23/255, green: 35/255, blue: 57/255, alpha: 1)
         // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
         self.navigationController?.navigationBar.tintColor = .white
         // ナビゲーションバーのテキストを変更する
@@ -61,9 +61,34 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             .foregroundColor: UIColor.white
         ]
         
+        if let navigationBar = self.navigationController?.navigationBar {
+            let gradient = CAGradientLayer()
+            var bounds = navigationBar.bounds
+            bounds.size.height += self.additionalSafeAreaInsets.top
+            gradient.frame = bounds
+            gradient.colors = [UIColor(red: 41/255, green: 162/255, blue: 226/255, alpha: 1).cgColor, UIColor(red: 65/255, green: 132/255, blue: 190/255, alpha: 1).cgColor]
+            gradient.startPoint = CGPoint(x: 0, y: 0)
+            gradient.endPoint = CGPoint(x: 1, y: 0)
+
+            if let image = getImageFrom(gradientLayer: gradient) {
+                navigationBar.setBackgroundImage(image, for: UIBarMetrics.default)
+            }
+        }
+        
         //キーボードの出現を感知
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func getImageFrom(gradientLayer:CAGradientLayer) -> UIImage? {
+        var gradientImage:UIImage?
+        UIGraphicsBeginImageContext(gradientLayer.frame.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            gradientLayer.render(in: context)
+            gradientImage = UIGraphicsGetImageFromCurrentImageContext()?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch)
+        }
+        UIGraphicsEndImageContext()
+        return gradientImage
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- MagotakuApp/Controller/Seniors/SignUp/SignUp0/SignUp0ViewController.swift
+++ MagotakuApp/Controller/Seniors/SignUp/SignUp0/SignUp0ViewController.swift
@@ -28,7 +28,7 @@ class SignUp0ViewController: UIViewController, UITextFieldDelegate {
         // navigationBarのタイトル
         self.title = "アカウント登録"
         //　ナビゲーションバーの背景色
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 23/255, green: 35/255, blue: 57/255, alpha: 1)
+//        self.navigationController?.navigationBar.barTintColor = UIColor(red: 23/255, green: 35/255, blue: 57/255, alpha: 1)
         // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
         self.navigationController?.navigationBar.tintColor = .white
         // ナビゲーションバーのテキストを変更する
@@ -50,6 +50,32 @@ class SignUp0ViewController: UIViewController, UITextFieldDelegate {
         //ボタンの角丸
         nextBtn.layer.cornerRadius = 6
         
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            let gradient = CAGradientLayer()
+            var bounds = navigationBar.bounds
+            bounds.size.height += self.additionalSafeAreaInsets.top
+            gradient.frame = bounds
+            gradient.colors = [UIColor(red: 41/255, green: 162/255, blue: 226/255, alpha: 1).cgColor, UIColor(red: 65/255, green: 132/255, blue: 190/255, alpha: 1).cgColor]
+            gradient.startPoint = CGPoint(x: 0, y: 0)
+            gradient.endPoint = CGPoint(x: 1, y: 0)
+
+            if let image = getImageFrom(gradientLayer: gradient) {
+                navigationBar.setBackgroundImage(image, for: UIBarMetrics.default)
+            }
+        }
+        
+    }
+    
+    func getImageFrom(gradientLayer:CAGradientLayer) -> UIImage? {
+        var gradientImage:UIImage?
+        UIGraphicsBeginImageContext(gradientLayer.frame.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            gradientLayer.render(in: context)
+            gradientImage = UIGraphicsGetImageFromCurrentImageContext()?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch)
+        }
+        UIGraphicsEndImageContext()
+        return gradientImage
     }
     
     override func viewDidLayoutSubviews() {
